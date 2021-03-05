3bbc1a4833e6c7a46cb44cdf43f96eab287fae24
donghoon-khan <mustwin@kakao.com>
2021-03-05 21:35:41 +0900

    Create project

---
+++ src/main/java/github/donghoonkhan/rabbitmq/rabbitMqManager/RabbitMqManagerApplication.java
@@ -0,0 +1,13 @@
+package github.donghoonkhan.rabbitmq.rabbitMqManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMqManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitMqManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/github/donghoonkhan/rabbitmq/rabbitMqManager/RabbitMqManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package github.donghoonkhan.rabbitmq.rabbitMqManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMqManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
